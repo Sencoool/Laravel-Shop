c3bd2ca416e594115763dac05898858ba1275f53
Sencoool <s6506021610037@email.kmutnb.ac.th>
2024-08-21 20:16:36 +0700

    New Laravel Project

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+<head>
+
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+<title>Testing</title>
+<link rel="stylesheet" href="{{ asset('vendor/bootstrap/css/bootstrap.min.css') }}">
+<script src="{{ asset('js/jquery-3.6.0.min.js') }}"></script>
+</head>
+
+<body>
+<script src="{{ asset('vendor/bootstrap/js/bootstrap.min.js') }}"></script>
+</body>
+</html>
